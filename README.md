90af9866891e268dfe6731ba0c6aac361510ae33
booyaa <booyaabooyaabooyaa@gmail.com>
2016-09-01 19:15:50 +0100

    :shipit:

---
+++ README.md
@@ -0,0 +1,23 @@
+# qwk
+
+travis-ci: [![Build Status](https://travis-ci.org/booyaa/qwk.svg?branch=master)](https://travis-ci.org/booyaa/qwk)
+
+A [Duck Duck Go](https://duckduckgo.com) client.
+
+# Install
+
+`cargo install qwk`
+
+Binaries to follow.
+
+# Usage
+
+`qwk foo`
+
+Searches for 'foo' on Duck Duck Go and returns the Abstract Text or if `!bang` command the redirected url.
+
+Copyright 2016 Mark Sta Ana.
+
+Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+http://www.apache.org/licenses/LICENSE-2.0> at your option. This file may not
+be copied, modified, or distributed except according to those terms.
---
+++ src/main.rs
@@ -0,0 +1,69 @@
+extern crate quackngo;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+extern crate clap;
+use clap::{Arg, App}; //, SubCommand};
+
+//     // &t=user-agent
+//
+//     // -type A/C/N/E - return 1 if nothing
+//     // -count N - return how many records, default is all
+//     // Abstracts and categories
+//     // - -rt = show related records instead (default for categories)
+//     // Abstracts only
+//     // - return abstract_text (default)
+//     // Exclusive only
+//     // - return redirect
+
+#[allow(unused_imports)]
+use quackngo::Quack;
+
+const APP_VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const APP_NAME: &'static str = env!("CARGO_PKG_NAME");
+const APP_AUTHOR: &'static str = env!("CARGO_PKG_AUTHORS");
+const APP_ABOUT: &'static str = env!("CARGO_PKG_DESCRIPTION");
+
+fn main() {
+    env_logger::init().unwrap();
+
+    let matches = App::new(APP_NAME)
+                      .version(APP_VERSION)
+                      .author(APP_AUTHOR)
+                      .about(APP_ABOUT)
+                      .arg(Arg::with_name("query")
+                               .help("Something to search")
+                               .required(true)
+                               .index(1))
+                      .arg(Arg::with_name("type")
+                               .help("Type of record to return, will error if no results")
+                               .short("t")
+                               .long("type")
+                               .value_name("A|C|N|E")
+                               .possible_values(&["A", "C", "N", "E"])
+                               .takes_value(true))
+                      .arg(Arg::with_name("count")
+                               .help("Number of records to return")
+                               .short("c")
+                               .long("count")
+                               .value_name("RECORDS")
+                               .takes_value(true))
+                      .get_matches();
+
+    let query = matches.value_of("query").unwrap();
+    println!("query: {}", query);
+
+    if matches.is_present("count") {
+        let count = matches.value_of("count").unwrap();
+        println!("count: {}", count);
+    }
+
+    if matches.is_present("type") {
+        let answer_type = matches.value_of("type").unwrap();
+        println!("type: {}", answer_type);
+    }
+
+
+    // let foo = Quack::new("!imdb rushmore");
+    // println!("{:#?}", foo);
+}
